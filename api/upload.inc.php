8a19bbf5ba6559ee5b53d68deffd6a692eb678b4
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-10-28 13:52:23 +0900

    fix: create dir

--- api/upload.inc.php
+++ api/upload.inc.php
@@ -8,6 +8,10 @@ if (!array_key_exists('oid', $_GET) || empty($_GET['oid'])) {
 }
 
 $path = 'data/objects'.$dir.$_GET['oid'];
+# create directory if it doesn't exist
+if(!file_exists('data/objects'.$dir)){
+	mkdir('data/objects'.$dir, 0777, true);
+}
 if(!file_exists($path)){
 	file_put_contents($path, file_get_contents('php://input'));
 }
