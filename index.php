6d4526daf97cbaa5ddef426a38c298adbe3fb07b
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-10-28 14:07:30 +0900

    fix

--- index.php
+++ index.php
@@ -25,26 +25,6 @@ function _slash_process($str)
 	$str = $str.'/';
 }
 
-function include_with_variables($filePath, $variables = array(), $print = true)
-{
-    // Extract the variables to a local namespace
-    extract($variables);
-
-    // Start output buffering
-    ob_start();
-
-    // Include the template file
-    include $filePath;
-
-    // End buffering and return its contents
-    $output = ob_get_clean();
-    if (!$print) {
-        return $output;
-    }
-    echo $output;
-}
-
-
 define('IN_GITLFS', true);
 require_once 'config.inc.php';
 
@@ -62,27 +42,25 @@ if (_str_ends_with($api, '/locks/verify'))
 {
 	$dir = _str_before($api, '/locks/verify');
 	$dir = _slash_process($dir);
-	// include 'api/locks_verify.inc.php';
-	include_with_variables('api/locks_verify.inc.php', array('dir' => $dir, 'server_url' => $server_url));
+	include 'api/locks_verify.inc.php';
 }
 elseif (_str_ends_with($api, '/objects/batch'))
 {
 	$dir = _str_before($api, '/objects/batch');
 	$dir = _slash_process($dir);
-	// include 'api/objects_batch.inc.php';
-	include_with_variables('api/objects_batch.inc.php', array('dir' => $dir, 'server_url' => $server_url));
+	echo "server_url: $server_url\n";
+	echo "dir: $dir\n";
+	include 'api/objects_batch.inc.php';
 }
 elseif (_str_ends_with($api, '/upload'))
 {
 	$dir = _str_before($api, '/upload');
 	$dir = _slash_process($dir);
-	// include 'api/upload.inc.php';
-	include_with_variables('api/upload.inc.php', array('dir' => $dir, 'server_url' => $server_url));
+	include 'api/upload.inc.php';
 }
 elseif (_str_ends_with($api, '/download'))
 {
 	$dir = _str_before($api, '/download');
 	$dir = _slash_process($dir);
-	// include 'api/download.inc.php';
-	include_with_variables('api/download.inc.php', array('dir' => $dir, 'server_url' => $server_url));
+	include 'api/download.inc.php';
 }
